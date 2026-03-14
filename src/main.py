c4b97ef7d52e596ddb57cca5e8dd004b3ada60e9
deepanjalkumar1 <deepanjalkumarkumar@gmail.com>
2026-03-14 09:07:14 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ async def greetings(name:str):
         }
 
 
-@app.get("/landingpage")
+@app.get("/")
 async def landingpageflow():
     '''
     landingpageflow() -> this function returns the greeting message to fellow user if ran successfully, else please try again message
